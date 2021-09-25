97923f5cbbfa00ed3d59c241a89196d9eb553ea9
bkevin1052 <31713984+bkevin1052@users.noreply.github.com>
2021-09-24 20:16:10 -0600

    AGREGANDO CAMBIOS DE BLOB

--- src/app/grafica-diaria/grafica-diaria.component.ts
+++ src/app/grafica-diaria/grafica-diaria.component.ts
@@ -1,9 +1,10 @@
 import { GraficaDiariaService } from './../services/GraficaDiaria/grafica-diaria.service';
-import { NgxMatDateAdapter, NgxMatDateFormats, NGX_MAT_DATE_FORMATS } from '@angular-material-components/datetime-picker';
+import { MatDatetimePickerInputEvent, NgxMatDateAdapter, NgxMatDateFormats, NgxMatDatetimePicker, NGX_MAT_DATE_FORMATS } from '@angular-material-components/datetime-picker';
 import { HttpClient } from '@angular/common/http';
 import { Component, NgModule, NgZone, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MAT_DATE_LOCALE, ThemePalette } from '@angular/material/core';
+import { DateSelectionModelChange, MatDatepickerInput } from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-grafica-diaria',
@@ -14,6 +15,31 @@ export class GraficaDiariaComponent implements OnInit {
 
   imagen: any;
 
+  selectedEstacion: string = '';
+  selectedSensor: string = '';
+  selectedDateTime: Date;
+
+  selectChangeHandler1 (event: any) {
+    //update the ui
+    this.selectedEstacion = event.target.value;
+    console.log(this.selectedEstacion)
+  }
+
+  selectChangeHandler2 (event: any) {
+    //update the ui
+    this.selectedSensor = event.target.value;
+    console.log(this.selectedSensor)
+  }
+
+  selectChangeHandler3 (event: any) {
+    //update the ui
+    this.selectedDateTime = event;
+    console.log(this.selectedDateTime)
+    this.graficaDiariaService.GetData(true).subscribe(res => {
+      this.createImageFromBlob(res);
+    })
+  }
+
   public formGroup = new FormGroup({
     date: new FormControl(null, [Validators.required]),
     date2: new FormControl(null, [Validators.required])
@@ -22,12 +48,12 @@ export class GraficaDiariaComponent implements OnInit {
   constructor(private graficaDiariaService: GraficaDiariaService) { }
 
   ngOnInit(): void {
-    this.graficaDiariaService.GetData(true).subscribe(res => {
+    this.graficaDiariaService.GetData(this.selectedEstacion).subscribe(res => {
       this.createImageFromBlob(res);
     })
   }
 
-  
+
 
 createImageFromBlob(image: Blob) {
        let reader = new FileReader();
