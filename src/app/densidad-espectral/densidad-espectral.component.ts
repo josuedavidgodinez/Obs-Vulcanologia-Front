729687b91eb0e1fed367be6955c6bb59af0df14f
bkevin1052 <31713984+bkevin1052@users.noreply.github.com>
2021-10-21 19:34:50 -0600

    Agregando ultima ruta para densidad espectral

--- src/app/densidad-espectral/densidad-espectral.component.ts
+++ src/app/densidad-espectral/densidad-espectral.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DensidadEspectralService } from './../services/DensidadEspectral/densidad-espectral.service';
 
 @Component({
   selector: 'app-densidad-espectral',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DensidadEspectralComponent implements OnInit {
 
-  constructor() { }
+  imagen: any;
+
+  constructor(private DensidadEspectralService: DensidadEspectralService) { }
 
   ngOnInit(): void {
+    this.DensidadEspectralService.GetData(true).subscribe(res => {
+      this.createImageFromBlob(res);
+    })
   }
+
+  createImageFromBlob(image: Blob) {
+    let reader = new FileReader();
+    reader.addEventListener("load", () => {
+       this.imagen = reader.result;
+    }, false);
+
+    if (image) {
+       reader.readAsDataURL(image);
+    }
+}
 
 }
