91eaf9d475b99f155e3f5b4fc037927427678bd3
bkevin1052 <31713984+bkevin1052@users.noreply.github.com>
2021-10-21 19:25:19 -0600

    Agregando componentes a modulo

--- src/app/espectrograma/espectrograma.module.ts
+++ src/app/espectrograma/espectrograma.module.ts
@@ -1,8 +1,15 @@
+
+import { MatNativeDateModule } from '@angular/material/core';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { EspectrogramaRoutingModule } from './espectrograma-routing.module';
 import { EspectrogramaComponent } from './espectrograma.component';
+import { NgxMatDatetimePickerModule, NgxMatTimepickerModule } from '@angular-material-components/datetime-picker';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -11,7 +18,16 @@ import { EspectrogramaComponent } from './espectrograma.component';
   ],
   imports: [
     CommonModule,
-    EspectrogramaRoutingModule
+    EspectrogramaRoutingModule,
+    NgxMatDatetimePickerModule,
+    NgxMatTimepickerModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatButtonModule,
+    MatNativeDateModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ]
 })
 export class EspectrogramaModule { }
