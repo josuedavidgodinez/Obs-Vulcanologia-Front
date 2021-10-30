d42e5a2466f3e17c6c4c4a6e83c4b1d42a262418
bkevin1052 <31713984+bkevin1052@users.noreply.github.com>
2021-10-29 18:51:57 -0600

    cambiando ruta por que estoy ppp

--- src/app/services/GraficaDiaria/grafica-diaria.service.ts
+++ src/app/services/GraficaDiaria/grafica-diaria.service.ts
@@ -2,8 +2,8 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { tap, catchError } from 'rxjs/operators';
-import { environment } from 'src/environments/environment';
-import { formatDate } from '@angular/common'; 
+import { environment } from 'src/environments/environment.prod';
+import { formatDate } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
--- src/app/services/GraficasInicio/graficas-inicio.service.ts
+++ src/app/services/GraficasInicio/graficas-inicio.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http'
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { tap, catchError } from 'rxjs/operators';
-import { environment } from '../../../environments/environment';
+import { environment } from '../../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
